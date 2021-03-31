308e39844cf3b9d86c058ad0ad601d6ae6788524
tempakyousuke <tempakyousuke@gmail.com>
2021-03-31 18:57:47 +0900

    add navigation

--- nuxt/layouts/default.vue
+++ nuxt/layouts/default.vue
@@ -1,62 +1,35 @@
 <template>
   <div>
-    <Nuxt />
+    <TopNavigation
+      @click="
+        () => {
+          console.log('test');
+          open = !open;
+        }
+      "
+    />
+    <nuxt />
+    <NavigationDrawer v-model="open" />
+    <div class="hidden opacity-0 opacity-50">
+      this element is instead of safelist.In the future, remove this element.
+    </div>
   </div>
 </template>
 
-<style>
-html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
+<script lang="ts">
+import { Vue, Component } from "nuxt-property-decorator";
+import TopNavigation from "@/components/TopNavigation/TopNavigation.vue";
+import NavigationDrawer from "@/components/NavigationDrawer/NavigationDrawer.vue";
 
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
+@Component({ components: { TopNavigation, NavigationDrawer } })
+export default class Default extends Vue {
+  private open = false;
+  console = console;
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+  created() {
+    if (!this.$fire.auth.currentUser) {
+      this.$router.push("/login/");
+    }
+  }
 }
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-</style>
+</script>
